c485df01546c61015a6a6b9d7061c3314b085ca8
Joseph-Lartey <josephlartey414@gmail.com>
2024-07-31 22:38:05 +0000

    validation

--- backend/index.php
+++ backend/index.php
@@ -9,7 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config/database.php';
 require_once __DIR__ . '/app/controllers/userController.php';
 require_once __DIR__ . '/app/controllers/ForgetPasswordController.php';
-require_once __DIR__ . '/app/middleware/ValidationMiddleWare.php';
+require_once __DIR__ . '/app/middleware/ValidationmiddleWare.php';
 require_once __DIR__ . '/app/controllers/ChangePasswordController.php';
 require_once __DIR__ . '/app/controllers/BookingController.php';
 require_once __DIR__ . '/app/controllers/CountryController.php';

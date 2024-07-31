8d23399d25ef692aa38c3f42e1aa1967820db08c
Joseph-Lartey <josephlartey414@gmail.com>
2024-07-31 23:07:20 +0000

    commit

--- backend/app/controllers/BookingController.php
+++ backend/app/controllers/BookingController.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '../../config/database.php';
+require_once __DIR__ . '../config/database.php';
 require_once __DIR__ . '../models/Flight.php';
 
 class BookingController {

8a04d0e536ea3f26ad356270a69d6e7c25d1ba88
Joseph-Lartey <josephlartey414@gmail.com>
2024-07-31 23:13:22 +0000

    path edit

--- backend/app/controllers/BookingController.php
+++ backend/app/controllers/BookingController.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../../config/database.php';
-require_once __DIR__ . '../models/Flight.php';
+require_once __DIR__ . '/../models/Flight.php';
 
 class BookingController {
     private $db;
